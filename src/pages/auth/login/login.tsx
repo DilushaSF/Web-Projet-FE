673b95f479c5f66023099bdedfd26b1b0c1ab675
romesh-CG <romeshperera97@gmail.com>
2025-07-15 22:49:37 +0100

    Login page added

---
+++ src/pages/auth/login/login.tsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
+export default function LoginPage() {
+  return (
+    <>
+      <Box sx={{ px: "20px", pb: 4, pt: 2.5 }}>
+        <Typography variant="h3" gutterBottom>
+          Login Page
+        </Typography>
+      </Box>
+    </>
+  );
+}
