2f7940e3f14534fdf454f341c97678f484e4836b
romesh-CG <romeshperera97@gmail.com>
2025-07-15 22:49:19 +0100

    Not found page added

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,16 @@
+// material
+import { Box, Typography } from "@mui/material";
+
+// ----------------------------------------------------------------------
+
+export default function NotFound() {
+  return (
+    <>
+      <Box sx={{ px: "20px", pb: 4, pt: 2.5 }}>
+        <Typography variant="h3" gutterBottom>
+          404 Page Not Found
+        </Typography>
+      </Box>
+    </>
+  );
+}
