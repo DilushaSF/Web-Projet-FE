1b24a22f2fa21a4e59380f4e6c3dc037d3fe882a
romesh-CG <romeshperera97@gmail.com>
2025-07-20 11:08:56 +0100

    minor change in Not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
   return (
     <>
       <Box sx={{ px: "20px", pb: 4, pt: 2.5 }}>
-        <Typography variant="h3" gutterBottom>
+        <Typography variant="h2" gutterBottom>
           404 Page Not Found
         </Typography>
       </Box>
