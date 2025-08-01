94d682f75feb48cd5cc19902d80f2706a09928fd
romesh-CG <romeshperera97@gmail.com>
2025-08-01 21:12:30 +0100

    Added: Notostack package added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import { SnackbarProvider } from "notistack";
 import Router from "./pages/router";
 import CustomTheme from "./pages/themes/theme";
 
 const App = () => (
   <ThemeProvider theme={CustomTheme}>
-    {/* <SnackbarProvider maxSnack={3}> */}
-    <CssBaseline />
-    <Router />
-    {/* </SnackbarProvider> */}
+    <SnackbarProvider maxSnack={3}>
+      <CssBaseline />
+      <Router />
+    </SnackbarProvider>
   </ThemeProvider>
 );
 
