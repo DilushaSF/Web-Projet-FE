a84b53bf177c2253df87dfcf77ada671b2481a30
LakjeewaWije <layerswije@gmail.com>
2025-08-03 00:08:29 +0100

    Updated : auth context with context types

--- src/pages/context/authContext.tsx
+++ src/pages/context/authContext.tsx
@@ -1,2 +1,12 @@
-// TODO: Add auth context here
-// TODO: Add Relevant Params
+import type { LoginParams, SignupParams } from "../../services/authService";
+import type { User } from "../../services/userService";
+
+// Auth Types
+type AuthContextType = {
+    user: User | null;
+    login: (credentials: LoginParams) => Promise<void>;
+    register: (userData: SignupParams) => Promise<void>;
+    logout: () => void;
+    isAuthenticated: boolean;
+    isLoading: boolean;
+};
