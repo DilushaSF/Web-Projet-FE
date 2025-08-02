d46766112bef586bf701489cdc8b2774e6f75dde
LakjeewaWije <layerswije@gmail.com>
2025-08-03 00:07:35 +0100

    Added : auth service with login and signup params

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,1 +1,14 @@
 // TODO : Add relevant Params Here
+export interface SignupParams {
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    mobilePhone: string;
+    password: string;
+    eircode: string;
+}
+
+export interface LoginParams {
+    mobilePhone: string;
+    password: string;
+}
