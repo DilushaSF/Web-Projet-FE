22ee6a3886fca08eea0320b0c3d1ad852d1d2759
romesh-CG <romeshperera97@gmail.com>
2025-08-03 04:34:56 +0100

    Updated: const file updated

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -6,3 +6,12 @@ export const categoryNames: { [key: number]: string } = {
   5: "Mandolin",
   6: "Piano",
 };
+
+export const brandNames: { [key: number]: string } = {
+  1: "Yamaha",
+  2: "Fender",
+  3: "Gibson",
+  4: "Casio",
+  5: "Rockstar",
+  6: "Roland",
+};
