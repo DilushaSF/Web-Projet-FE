fb91d69f58da3e17577126f3d5f008d82da01a4e
romesh-CG <romeshperera97@gmail.com>
2025-08-07 22:01:38 +0100

    Modified: Wrapped componenents inside auth context

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,14 @@ import "./index.css";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { AuthProvider } from "./pages/context/authContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </StrictMode>
 );
