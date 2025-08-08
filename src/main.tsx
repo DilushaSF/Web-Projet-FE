fd9dbbd5b1379cd7ff6f89fe2dc3d72caaff622c
romesh-CG <romeshperera97@gmail.com>
2025-08-08 17:09:52 +0100

    Modified: Main file with react query lib import"

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,27 @@ import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AuthProvider } from "./pages/context/authContext.tsx";
+import { CartProvider } from "./pages/context/cartContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <CartProvider>
+          <Router>
+            <App />
+          </Router>
+        </CartProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
