9f8e406e09311bdbc43dc6541d50f6c096e42a0d
Dilusha Sindutharu <sindutharu821@gmail.com>
2025-08-08 22:04:23 +0100

    Footer adding changes

--- src/pages/layout/pageLayout.tsx
+++ src/pages/layout/pageLayout.tsx
@@ -1,28 +1,27 @@
 import React from "react";
-import { Box, Container } from "@mui/material";
-import { Navbar } from "./Navbar";
+import {Box, Container} from "@mui/material";
+import {Navbar} from "./Navbar";
+import {Footer} from "./Footer";
 
 interface PageLayoutProps {
   children: React.ReactNode;
 }
 
-const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
+const PageLayout: React.FC<PageLayoutProps> = ({children}) => {
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
         minHeight: "100vh",
-      }}
-    >
+      }}>
       <Navbar />
-      <Box component="main" sx={{ flexGrow: 1, mt: { xs: 8, sm: 9 } }}>
-        {/* Wrap children in a responsive container */}
-        <Container maxWidth="lg" sx={{ py: 4 }}>
+      <Box component="main" sx={{flexGrow: 1, mt: {xs: 8, sm: 9}}}>
+        <Container maxWidth="lg" sx={{py: 4}}>
           {children}
         </Container>
       </Box>
-      {/* <Footer /> */}
+      <Footer />
     </Box>
   );
 };
