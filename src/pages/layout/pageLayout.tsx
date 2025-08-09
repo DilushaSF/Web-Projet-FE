df75d33a75b3cddab1a48dea58242e8d577b4b5f
romesh-CG <romeshperera97@gmail.com>
2025-08-09 15:02:28 +0100

    Updated: Layout page changes

--- src/pages/layout/pageLayout.tsx
+++ src/pages/layout/pageLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Container } from "@mui/material";
-import { Navbar } from "./Navbar";
+import { Navbar } from "./navbar";
 import { Footer } from "./footer";
 
 interface PageLayoutProps {
